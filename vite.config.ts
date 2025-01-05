864dbff582a7a9747c9823a37b230c83df29ec53
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2025-01-05 10:38:31 +0100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
-	plugins: [wasm(), sveltekit()],
+	plugins: [wasm(), topLevelAwait(), sveltekit()],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
